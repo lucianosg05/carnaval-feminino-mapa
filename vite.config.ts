60177695f3566e7b576c097c4bf190ae58c6d633
lucianosg05 <lsoares20357@gmail.com>
2025-09-19 17:53:33 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
 
+// Corrige o dirname (não existe em ESM)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "::",
